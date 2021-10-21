03c854dcfddd2b84a4190f54ca73402020805208
Torwent <dinisgrilo@gmail.com>
2021-10-21 17:40:14 +0200

    changed single quotes to double quotes

--- commands/admin/serverinfo.js
+++ commands/admin/serverinfo.js
@@ -1,9 +1,9 @@
 const { DiscordAPIError, MessageEmbed } = require("discord.js")
 
 module.exports = {
-    commands: ['serverinfo', 'sinfo', 'si'],
-    expectedArgs: '',
-    permissionError: 'You need admin permissions to run this command',
+    commands: ["serverinfo", "sinfo", "si"],
+    expectedArgs: "",
+    permissionError: "You need admin permissions to run this command",
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
@@ -23,6 +23,6 @@ module.exports = {
             
             message.channel.send({ embeds: [siEmbed] })
     },
-    permissions: ['ADMINISTRATOR'],
+    permissions: ["ADMINISTRATOR"],
     requiredRoles: [],
 }
--- commands/template.js
+++ commands/template.js
@@ -1,7 +1,7 @@
 module.exports = {
-    commands: ['template'],
-    expectedArgs: '',
-    permissionError: 'You need admin permissions to run this command',
+    commands: ["template"],
+    expectedArgs: "",
+    permissionError: "You need admin permissions to run this command",
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
