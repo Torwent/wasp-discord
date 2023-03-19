bb56669c749a4ee83d66fbb6a7f0426858cbeab0
Torwent <Torwent@users.noreply.github.com>
2023-03-19 10:55:58 +0100

    fix: added more debugging to found what's crashing the bot

--- src/structures/WebSocket.ts
+++ src/structures/WebSocket.ts
@@ -5,17 +5,26 @@ const wss = new webSocket.Server({ port: 4100 })
 
 export const wssListen = async (client: ExtendedClient) => {
   console.log("Listening for websocket connections on port 4100!")
+
   wss.on("connection", (ws: webSocket) => {
     console.log("New client connected!")
 
     ws.on("message", (data: any) => {
       try {
-        let user_id = `${data}`
-        let guild = client.guilds.resolve(process.env.GUILD_ID)
-        let member = guild.members.cache.get(user_id)
-        let memberRoles = member.roles.cache
-          .filter((roles: { id: string }) => roles.id !== guild.id)
-          .map((role: { toString: () => any }) => role.toString())
+        const user_id = `${data}`
+        console.log("Requesting: ", user_id)
+        console.log("ClientIsReady: ", client.isReady)
+        const guild = client.guilds.resolve(process.env.GUILD_ID)
+        console.log("guildname: ", guild.name)
+        const member = guild.members.cache.get(user_id)
+        console.log("member name: ", member.displayName)
+        let memberRoles: string[] = []
+
+        if (member != null)
+          memberRoles = member.roles.cache
+            .filter((roles: { id: string }) => roles.id !== guild.id)
+            .map((role: { toString: () => any }) => role.toString())
+
         ws.send(memberRoles.toString())
       } catch (error) {
         console.error(error)
