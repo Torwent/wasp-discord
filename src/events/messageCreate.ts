3553c0b3431c9e5d36610116a2cb09081c269b7c
Torwent <Torwent@users.noreply.github.com>
2024-10-23 16:25:58 +0200

    fix: simplify message screening and added some more information when messages get deleted

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,41 +1,35 @@
 import { ClientEvent } from "$lib/event"
 import { achievements, bans } from "$lib/lib"
-import { Message, MessageType, TextChannel } from "discord.js"
-
-// Cooldown map to store the last execution time for each user
-const cooldowns = new Map()
-// Variable to store the last global execution time
-const lastGlobalExecution = 0
-
-function getEnding(n: number) {
-	return "This message will self-destruct in **" + n + "** seconds."
-}
-
-async function timedReply(reply: Message, msg: string, n: number) {
-	if (n < 1) {
-		await reply.delete()
-		return
-	}
-
-	await reply.edit(msg + getEnding(n))
-	setTimeout(async () => await timedReply(reply, msg, n - 1), 1000)
-}
+import { MessageType, TextChannel } from "discord.js"
 
 export default new ClientEvent("messageCreate", async (message) => {
 	if (message.author.bot) return
 
 	if (message.attachments.size > 0) {	
 		for (const attachment of message.attachments.values()) {
-			console.log(attachment)
 			const contentType = attachment.contentType || attachment.name
 
 			if (contentType.startsWith("audio") || /\.(mp3|wav|ogg|flac)$/i.test(contentType)) {
+				let msg = "<@" + message.author.id + "> your message has been deleted.\n\n"
+				msg += "The file type `" + contentType + "` is not allowed on this server. If this seems like a mistake contact a moderator and let them know 😄\n\n"
+				msg += "This message will self-destruct in **15** seconds."
+				const reply = await message.reply(msg)
 				await message.delete()
+				
+				setTimeout(async () => await reply.delete(), 15000)
 				return
 			}
 
-			if ((!contentType.startsWith("image") || contentType === "image/gif") && (message.channel === achievements)) {
+			if (message.channel !== achievements) continue;
+
+			if ((!contentType.startsWith("image") && !contentType.startsWith("video")) || contentType === "image/gif") {		
+				let msg = "<@" + message.author.id + "> your message has been deleted.\n\n"
+				msg += "The file type `" + contentType + "` is not allowed on this channel. If this seems like a mistake contact a moderator and let them know 😄\n\n"
+				msg += "This message will self-destruct in **15** seconds."
+				const reply = await message.reply(msg)
 				await message.delete()
+				
+				setTimeout(async () => await reply.delete(), 15000)
 				return
 			}
 		}
@@ -48,23 +42,22 @@ export default new ClientEvent("messageCreate", async (message) => {
 		if (message.attachments.size === 0) {
 			let msg = "<@" + message.author.id + "> your message has been deleted.\n\n"
 			msg += "This is a media only server, please post a picture of your achievement 😄\n\n"
-			let n = 15
-			const reply = await message.reply(msg + getEnding(n--))
+			msg += "This message will self-destruct in **15** seconds."
+			const reply = await message.reply(msg)
 			await message.delete()
 			
-			await timedReply(reply, msg, n)
+			setTimeout(async () => await reply.delete(), 15000)
 			return
 		}
 		
 		if ((message.type === MessageType.Reply)) {
 			let msg = "<@" + message.author.id + "> your message has been deleted.\n\n"
 			msg += "Please keep conversations within threads.\n\n"
-			
-			let n = 30
-			const reply = await message.reply(msg + getEnding(n--))
+			msg += "This message will self-destruct in **30** seconds."
+
+			const reply = await message.reply(msg)
 			await message.delete()
-			
-			await timedReply(reply, msg, n)
+			setTimeout(async () => await reply.delete(), 30000)
 			return
 		}
 
@@ -95,12 +88,11 @@ export default new ClientEvent("messageCreate", async (message) => {
 			msg += "Previous bans:\n"
 			msg += "Ban type:\n"
 			msg += "```\n\n"
+			msg += "This message will self-destruct in **30** seconds."
 
-			let n = 30
-			const reply = await message.reply(msg + getEnding(n--))
+			const reply = await message.reply(msg)
 			await message.delete()
-
-			await timedReply(reply, msg, n)
+			setTimeout(async () => await reply.delete(), 30000)
 			return
 		}
 
